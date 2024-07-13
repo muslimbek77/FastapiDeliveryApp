fa363500cce98c3585afb41fad17efdaf38fd80b
muslimbek77 <muslimjonbekali@mail.ru>
2024-07-13 16:27:32 +0500

    Post methodi yordamida Order yaratish

--- order_routes.py
+++ order_routes.py
@@ -11,6 +11,9 @@ order_router = APIRouter(
     prefix='/order'
 )
 
+session = session(bind=engine)
+
+
 @order_router.get('/')
 async def order(Authorize:AuthJWT=Depends()):
     try:
@@ -19,3 +22,29 @@ async def order(Authorize:AuthJWT=Depends()):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid token")
 
     return {"message" : "Bu order route sahifasi"}
+
+@order_router.post('/make',status_code=status.HTTP_201_CREATED)
+async def make_order(order:OrderModel,Authorize:AuthJWT=Depends()):
+    try:
+        Authorize.jwt_required()
+    except:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid token")
+    current_user = Authorize.get_jwt_subject()
+    user = session.query(User).filter(User.username == current_user).first()
+
+    new_order = Order(
+        quantity = order.quantity,
+        # product = order.product_id,
+
+    )
+    new_order.user = user
+    session.add(new_order)
+    session.commit()
+
+    response = {
+        "id":new_order.id,
+        "quantity":new_order.quantity,
+        "order_statuses":new_order.order_statuses,
+
+    }
+    return jsonable_encoder(response)
